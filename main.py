5888206019771625a51ff913db1b790ab53223c0
Kristián Sokol <kikosokol@gmail.com>
2022-03-22 04:38:14 +0100

    feat: use viola jones function in main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import save_video as sv
 
+import viola_jones as vj
+
 FOLDER_WITH_NPZ = "viz_vzorka/"
 FOLDER_WITH_MP4 = "mp4/"
 
@@ -8,16 +10,31 @@ npz_files = ["Kieran_Culkin_0.npz", "Liu_Ye_2.npz", "Maggie_Smith_3.npz", "Marga
              "Marisa_Tomei_1.npz", "Martin_Sheen_3.npz", "Martin_Sheen_5.npz", "Matt_Anderson_2.npz",
              "Natalie_Stewart_2.npz", "Oscar_Elias_Biscet_0.npz"]
 
+# for file_name in npz_files:
+#     video_file = np.load(FOLDER_WITH_NPZ + file_name)
+#     file_name_without_suffix = file_name.split(".")[0]
+#     directory_name = FOLDER_WITH_MP4 + file_name_without_suffix + "/"
+#     sv.to_mp4(directory_name, file_name_without_suffix, "ORIGINAL", video_file["colorImages_original"],
+#               video_file["landmarks2D"],
+#               video_file["boundingBox"])
+#     sv.to_mp4(directory_name, file_name_without_suffix, "MEDIUM", video_file["colorImages_medium"],
+#               video_file["landmarks2D"],
+#               video_file["boundingBox"])
+#     sv.to_mp4(directory_name, file_name_without_suffix, "SEVERE", video_file["colorImages_severe"],
+#               video_file["landmarks2D"],
+#               video_file["boundingBox"])
+
+###########################Viola-Jones###################################################
 for file_name in npz_files:
     video_file = np.load(FOLDER_WITH_NPZ + file_name)
     file_name_without_suffix = file_name.split(".")[0]
-    directory_name = FOLDER_WITH_MP4 + file_name_without_suffix + "/"
-    sv.to_mp4(directory_name, file_name_without_suffix, "ORIGINAL", video_file["colorImages_original"],
+    directory_name = file_name_without_suffix + "/"
+    vj.to_mp4(FOLDER_WITH_MP4, directory_name, file_name_without_suffix, "ORIGINAL", video_file["colorImages_original"],
               video_file["landmarks2D"],
               video_file["boundingBox"])
-    sv.to_mp4(directory_name, file_name_without_suffix, "MEDIUM", video_file["colorImages_medium"],
+    vj.to_mp4(FOLDER_WITH_MP4, directory_name, file_name_without_suffix, "MEDIUM", video_file["colorImages_medium"],
               video_file["landmarks2D"],
               video_file["boundingBox"])
-    sv.to_mp4(directory_name, file_name_without_suffix, "SEVERE", video_file["colorImages_severe"],
+    vj.to_mp4(FOLDER_WITH_MP4, directory_name, file_name_without_suffix, "SEVERE", video_file["colorImages_severe"],
               video_file["landmarks2D"],
               video_file["boundingBox"])
