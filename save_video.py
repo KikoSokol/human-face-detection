9997a65ba1a21f6e20db3860cbaef126c168f92c
Kristián Sokol <kikosokol@gmail.com>
2022-03-22 04:44:45 +0100

    feat: add color as parameter for write landmarks and bounding box

--- save_video.py
+++ save_video.py
@@ -4,25 +4,25 @@ import numpy as np
 import cv2
 
 
-def add_bounding_box(bounding_box, image):
+def add_bounding_box(bounding_box, image, color):
     for up in range(int(bounding_box[0][1]), int(bounding_box[1][1]) + 1):
-        image[up][int(bounding_box[0][0])] = np.array([0, 255, 0])
+        image[up][int(bounding_box[0][0])] = np.array(color)
 
     for down in range(int(bounding_box[2][1]), int(bounding_box[3][1]) + 1):
-        image[down][int(bounding_box[2][0])] = np.array([0, 255, 0])
+        image[down][int(bounding_box[2][0])] = np.array(color)
 
     for left in range(int(bounding_box[0][0]), int(bounding_box[2][0]) + 1):
-        image[int(bounding_box[0][1])][left] = np.array([0, 255, 0])
+        image[int(bounding_box[0][1])][left] = np.array(color)
 
     for right in range(int(bounding_box[1][0]), int(bounding_box[3][0]) + 1):
-        image[int(bounding_box[1][1])][right] = np.array([0, 255, 0])
+        image[int(bounding_box[1][1])][right] = np.array(color)
 
     return image
 
 
-def add_landmarks(landmarks, image):
+def add_landmarks(landmarks, image, color):
     for i in landmarks:
-        image[int(i[1])][int(i[0])] = np.array([0, 0, 255])
+        image[int(i[1])][int(i[0])] = np.array(color)
 
     return image
 
@@ -48,8 +48,8 @@ def to_mp4(directory, name, type, video, landmarks, bounding_box):
         landmark_image = landmarks[:, :, i]
         bounding_box_image = bounding_box[:, :, i]
 
-        img = add_landmarks(landmark_image, img)
-        img = add_bounding_box(bounding_box_image, img)
+        img = add_landmarks(landmark_image, img, [0, 0, 255])
+        img = add_bounding_box(bounding_box_image, img, [0, 255, 0])
 
         out.write(img)
 
