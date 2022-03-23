ca8e3e9b1cda536ca9cc45d47f5bd04b446e602f
Kristián Sokol <kikosokol@gmail.com>
2022-03-23 04:02:39 +0100

    feat: create file with tp, fp, fn, precision and recall data for all video image in CNN method

--- cnn.py
+++ cnn.py
@@ -6,21 +6,37 @@ import helper as hp
 DIRECTORY_CNN = "cnn/"
 
 
-def find_faces(img):
+def find_faces(img, correct_bounding_box):
 
     detector = MTCNN()
     faces = detector.detect_faces(img)
 
-    coordinates = []
+    tp = 0
+    fp = 0
+    fn = 0
+
     for face in faces:
         column, row, width, height = face["box"]
         coordinate = hp.get_four_vertices(column, row, width, height)
-        coordinates.append(coordinate)
-        img = hp.add_bounding_box(coordinate, img, [48, 88, 247])
+
+        iou = hp.compute_squares_iou(coordinate, correct_bounding_box)
+
+        if iou > 0.5:
+            tp += 1
+            img = hp.add_bounding_box(coordinate, img, [48, 88, 247])
+        else:
+            fp += 1
+            img = hp.add_bounding_box(coordinate, img, [0, 0, 255])
+
+    if tp == 0:
+        fn = 1
+
+    precision = round(tp / (tp + fp), 2)
+    recall = round(tp / (tp + fn), 2)
 
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    return img, coordinates
+    return img, hp.create_info_data(tp, fp, fn, precision, recall)
 
 
 def to_mp4(main_directory, directory, name, type, video, landmarks, bounding_box):
@@ -29,17 +45,22 @@ def to_mp4(main_directory, directory, name, type, video, landmarks, bounding_box
 
     out = cv2.VideoWriter(final_name, cv2.VideoWriter_fourcc(*'mp4v'), 25, frameSize, True)
 
+    all_info_data = []
+
     for i in range(video.shape[3]):
         img = video[:, :, :, i]
 
-        img, coordinates_found_bounding_box = find_faces(img)
-
         landmark_image = landmarks[:, :, i]
         bounding_box_image = bounding_box[:, :, i]
+
+        img, info_data = find_faces(img, bounding_box_image)
+        all_info_data.append(info_data)
 
         img = hp.add_landmarks(landmark_image, img, [0, 0, 255])
         img = hp.add_bounding_box(bounding_box_image, img, [0, 255, 0])
 
         out.write(img)
+
+    hp.create_info_file(main_directory, DIRECTORY_CNN, directory, name, type, all_info_data)
 
     out.release()
