9807054c9d596abae38a3ad25826847c21b7493e
Kristián Sokol <kikosokol@gmail.com>
2022-03-23 03:07:26 +0100

    feat: compute precision and recall in viola jones method

--- helper.py
+++ helper.py
@@ -42,6 +42,39 @@ def get_four_vertices(column, row, width, height):
     return [x0, x1, x2, x3]
 
 
+def compute_iou(found_face_square, correct_square):
+    x_inter1 = max(found_face_square[0], correct_square[0])
+    y_inter1 = max(found_face_square[1], correct_square[1])
+    x_inter_2 = min(found_face_square[2], correct_square[2])
+    y_inter_2 = min(found_face_square[3], correct_square[3])
+
+    width_inter = abs(x_inter_2 - x_inter1)
+    height_inter = abs(y_inter_2 - y_inter1)
+    area_inter = width_inter * height_inter
+    width_box1 = abs(found_face_square[2] - found_face_square[0])
+    height_box1 = abs(found_face_square[3] - found_face_square[1])
+    width_box2 = abs(correct_square[2] - correct_square[0])
+    height_box2 = abs(correct_square[3] - correct_square[1])
+
+    area_box1 = width_box1 * height_box1
+    area_box2 = width_box2 * height_box2
+    area_union = area_box1 + area_box2 - area_inter
+
+    return area_inter / area_union
+
+
+def get_cord(square_vertices):
+    return square_vertices[0][1], square_vertices[0][0], square_vertices[3][1], square_vertices[3][0]
+
+
+def compute_squares_iou(vertices_found_face_square, vertices_correct_square):
+    return compute_iou(get_cord(vertices_found_face_square), get_cord(vertices_correct_square))
+
+
+def create_info_data(tp, fp, fn, precision, recall):
+    return {"tp": tp, "fp": fp, "fn": fn, "precision": precision, "recall": recall}
+
+
 def create_directory_and_get_file_name(main_directory, category_directory, directory, name, type):
     dir = main_directory + category_directory
     create_folder(dir)
